80350edc1ffe1d28b5cd004a71d699187ca0e7df
german <germanjongewaard@gmail.com>
2017-11-03 12:12:04 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -107,6 +107,14 @@ public class String_2 {
    // Return true if the given string contains an appearance of "xyz" where 
    // the xyz is not directly preceeded by a period (.). So "xxyz" counts 
    // but "x.xyz" does not.     
+   public boolean xyzThere(String str)
+   { 
+  
+    
+    
+    
+    
+    
     
     
 }
