bd9a9dfdf962d28744713f15c1d9bca822ad9523
german <germanjongewaard@gmail.com>
2017-11-15 10:10:16 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -266,7 +266,8 @@ public class String_1 {
     
     // Given a string, return a new string made of 3 copies of the first 2 chars of the original string.
     // The string may be any length. If there are fewer than 2 chars, use whatever is there.
-    
+    public String extraFront(String str)
+{
     
     
     
