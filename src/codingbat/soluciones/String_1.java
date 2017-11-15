44b509b8d3e5b472dd548fc5b5d39ef11a9365a9
german <germanjongewaard@gmail.com>
2017-11-15 10:09:21 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -264,6 +264,9 @@ public class String_1 {
                 return (a + b.substring(lenB-lenA));
     }
     
+    // Given a string, return a new string made of 3 copies of the first 2 chars of the original string.
+    // The string may be any length. If there are fewer than 2 chars, use whatever is there.
+    
     
     
     
