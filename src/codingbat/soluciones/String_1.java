c1d9f5ca2a8c2fbb980ed25d6ba260361c1828ec
german <germanjongewaard@gmail.com>
2017-11-17 10:05:04 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -251,6 +251,9 @@ public class String_1 {
         return false;
     }
     
+    // Given two strings, append them together (known as "concatenation") and return the result.
+    // However, if the strings are different lengths, omit chars from the longer string so it is the
+    // same length as the shorter string. So "Hello" and "Hi" yield "loHi". The strings may be any length.
     
     
     
