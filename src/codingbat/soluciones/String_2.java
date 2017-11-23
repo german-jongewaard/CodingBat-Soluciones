92a1bbea85fbacedb964bd0f386bc1b377347ed1
german <germanjongewaard@gmail.com>
2017-11-23 12:02:39 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -78,6 +78,12 @@ public class String_2 {
             }
             return times;
     }
+    
+    // Given two strings, return true if either of the strings appears at the 
+    // very end of the other string, ignoring upper/lower case differences 
+    // (in other words, the computation should not be "case sensitive"). 
+    // Note: str.toLowerCase() returns the lowercase version of a string. 
+     
         
         
         
