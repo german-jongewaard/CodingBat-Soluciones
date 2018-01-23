961e764ac8401a1bccbcc347f27e015345bd76ab
german <germanjongewaard@gmail.com>
2018-01-23 13:58:50 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -309,5 +309,6 @@ public class Array_1 {
     
     // Given 2 int arrays, a and b, of any length, return a new array with 
     // the first element of each array.
+    // If either array is length 0, ignore that array.
 
 }
