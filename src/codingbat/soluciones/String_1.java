7f8da81a860c53b4559ed42102e268ff597d3090
german <germanjongewaard@gmail.com>
2018-03-13 17:47:05 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -80,6 +80,14 @@ public class String_1 {
             return (str.substring(len) + str.substring(0, len));
     }
     
+    // Given a string, return a string length 1 from its front, unless front is false,
+    // in which case return a string length 1 from its back. The string will be non-empty.
+    public String theEnd(String str, boolean front)
+    {
+            if(front)
+                    return str.substring(0, 1);
+            return str.substring(str.length()-1);
+    }
 
     
     
