c9b29f72644fd84f2c3a980daf64293db87aa51c
german <germanjongewaard@gmail.com>
2018-03-13 16:10:52 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/CodingBatSoluciones.java
+++ src/codingbat/soluciones/CodingBatSoluciones.java
@@ -32,6 +32,15 @@ public String makeOutWord(String out, String word)
 { return (out.substring(0, 2) + word + out.substring(2, 4)); }
 
 
+// Given a string, return the string made of its first two chars, so the String "Hello" yields "He".
+// If the string is shorter than length 2, return whatever there is, so "X" yields "X",
+// and the empty string "" yields the empty string "".Note that str.length() returns the length of a string.
+public String firstTwo(String str)
+{
+	if(str.length() >= 3)
+		return str.substring(0, 2);
+	return str;
+}
 
 
 }
