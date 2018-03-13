74700ac1b65eba2bebd4cacf2f81d2289d63290c
german <germanjongewaard@gmail.com>
2018-03-13 21:42:35 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -100,6 +100,15 @@ public class String_1 {
     }
     
     
+    // Given a string of even length, return a string made of the middle two chars,
+    // so the string "string" yields "ri". The string length will be at least 2.
+    public String middleTwo(String str) 
+    {
+            int half = str.length()/2-1;
+            return str.substring(half, half+2);
+    }
+    
+    
 }
 
 
