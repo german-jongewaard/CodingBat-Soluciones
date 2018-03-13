4b7ea3cc2f1fbc8080eaf33216b763112c42953e
german <germanjongewaard@gmail.com>
2018-03-13 16:04:52 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/CodingBatSoluciones.java
+++ src/codingbat/soluciones/CodingBatSoluciones.java
@@ -23,5 +23,15 @@ public String makeAbba(String a, String b)
 // Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
 public String makeTags(String tag, String word)
 { return ('<' + tag + '>' + word + '<' + '/' + tag + '>'); }
-    
+ 
+
+// Given an "out" string length 4, such as "<<>>", and a word, return a new string
+// where the word is in the middle of the out string, e.g. "<<word>>".
+// Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
+public String makeOutWord(String out, String word)
+{ return (out.substring(0, 2) + word + out.substring(2, 4)); }
+
+
+
+
 }
