c40194ea30f5377fe0dee37a175b186dc889406d
german <germanjongewaard@gmail.com>
2018-03-14 23:20:43 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -160,6 +160,11 @@ public class String_1 {
     }
     
     
+    // Given a string, return a string length 2 made of its first 2 chars.
+    // If the string length is less than 2, use '@' for the missing chars.
+    public String atFirst(String str)
+    
+    
 }
 
 
