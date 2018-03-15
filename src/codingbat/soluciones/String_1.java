923c1abba24f326aba5843552f728fff2b9893ef
german <germanjongewaard@gmail.com>
2018-03-15 16:42:54 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -190,7 +190,10 @@ public class String_1 {
     // Given two strings, append them together (known as "concatenation") and return the result.
     // However, if the concatenation creates a double-char, 
     //then omit one of the chars, so "abc" and "cat" yields "abcat". 
-    
+    public String conCat(String a, String b)
+    {
+        
+    }
     
     
 }
