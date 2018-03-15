3b5868e67e1f1eb86d69147cba1ecf5b009ef683
german <germanjongewaard@gmail.com>
2018-03-15 16:40:44 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -174,6 +174,15 @@ public class String_1 {
 		return "@@";
     }
     
+    
+    / Given 2 strings, a and b, return a new string made of the first char of a and
+    // the last char of b, so "yo" and "java" yields "ya". If either string is length 0
+    // use '@' for its missing char. 
+    public String lastChars(String a, String b)
+    {}
+    
+    
+    
 }
 
 
