a93667de926e408c6c3f372ced819f10e76de407
german <germanjongewaard@gmail.com>
2018-03-16 09:58:18 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -243,9 +243,13 @@ public class String_1 {
     // Given a string, return true if the first 2 chars in the string also appear
     // at the end of the string, such as with "edited".
     public boolean frontAgain(String str)
-{
-    
-}
+    {
+        int len = str.length();
+            if(len >= 2)
+                    return str.substring(0, 2).equals(str.substring(len-2, len));
+            else
+        return false;
+    }
     
     
     
