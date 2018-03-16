6c9da21e72ac1061d4d13a5aa5649ea8d3e9175a
german <germanjongewaard@gmail.com>
2018-03-16 09:53:13 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -206,7 +206,8 @@ public class String_1 {
     
     // Given a string of any length, return a new string where the last 2 chars
     // if present, are swapped, so "coding" yields "codign".
-    
+    public String lastTwo(String str)
+    {}
     
     
     
