2829822469cb4ab40f48074e0e40aab860e90332
german <germanjongewaard@gmail.com>
2018-03-16 09:55:18 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -217,7 +217,10 @@ public class String_1 {
     
     // Given a string, if the string begins with "red" or "blue" return that color string
     // otherwise return the empty string.
-    
+    public String seeColor(String str)
+    {
+        
+    }
     
     
     
