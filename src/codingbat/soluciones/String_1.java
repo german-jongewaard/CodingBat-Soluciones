d5dc2c116d9af8c65c35d5a5c9bc3930b3c2f79d
german <germanjongewaard@gmail.com>
2018-03-16 09:57:24 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -240,6 +240,9 @@ public class String_1 {
 		return "";
     }
     
+    // Given a string, return true if the first 2 chars in the string also appear
+    // at the end of the string, such as with "edited".
+    
     
     
     
