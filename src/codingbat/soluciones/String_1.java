77860265b8ca95d018541cfca312e958f70b8c9f
german <germanjongewaard@gmail.com>
2018-03-17 13:45:48 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -279,7 +279,7 @@ public class String_1 {
     // Given a string, if a length 2 substring appears at both its beginning and end,
     // return a string without the substring at the beginning, so "HelloHe" yields "lloHe".
     // The substring may overlap with itself, so "Hi" yields "". Otherwise, return the original string unchanged.
-    
+    public String without2(String str)
     
     
 }
