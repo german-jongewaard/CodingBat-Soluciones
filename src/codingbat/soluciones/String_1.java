8d0824f8576a5164bd4689437a59d686257c875f
german <germanjongewaard@gmail.com>
2018-03-18 13:54:21 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -295,7 +295,7 @@ public class String_1 {
     
     // Given a string, return a version without the first 2 chars. Except keep the first char
     // if it is 'a' and keep the second char if it is 'b'. The string may be any length. Harder than it looks.      
-    
+    public String deFront(String str)
     
 }
 
