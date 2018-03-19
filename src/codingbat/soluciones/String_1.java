6c3d385cd3bbd623d4eac26011cb86e22694799f
german <germanjongewaard@gmail.com>
2018-03-19 14:01:00 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -305,8 +305,13 @@ public class String_1 {
                 stbuild.append('a');
             if(str.charAt(1) == 'b')
                 stbuild.append('b');
-            stbuild.append(str.substring(2));
-        }
+            stbuild.append(str.substring(2));return stbuild.toString();
+	}
+	else if(len == 1 && str.charAt(0) == 'a')
+		return "a";
+	else
+		return "";
+    }
     
 }
 
