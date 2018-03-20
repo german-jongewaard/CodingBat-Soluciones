fb300bc9fd9510005061fa1bf90cb9bd3b49cc59
german <germanjongewaard@gmail.com>
2018-03-20 20:09:54 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -314,7 +314,9 @@ public class String_1 {
     // the front of the string, except its first char does not need to match exactly. On a match, return the
     // front of the string, or otherwise return the empty string. So, so with the string "hippo" the word
     // "hi" returns "hi" and "xip" returns "hip". The word will be at least length 1.
-    
+    public String startWord(String str, String word)
+{
+int lenStr = str.length();
 }
 
 
