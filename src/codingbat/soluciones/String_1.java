b13830c57a45a7e04ed09913828118d0cf1d7f44
german <germanjongewaard@gmail.com>
2018-03-20 20:10:52 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -315,8 +315,16 @@ public class String_1 {
     // front of the string, or otherwise return the empty string. So, so with the string "hippo" the word
     // "hi" returns "hi" and "xip" returns "hip". The word will be at least length 1.
     public String startWord(String str, String word)
-{
-int lenStr = str.length();
+    {
+        int lenStr = str.length();
+        int lenWord = word.length();
+                String temp;
+        if(lenStr >= lenWord)
+            {
+		temp = str.substring(1, lenWord);
+		if(word.substring(1).equals(temp))
+			return (str.charAt(0)+temp);
+                else
 }
 
 
