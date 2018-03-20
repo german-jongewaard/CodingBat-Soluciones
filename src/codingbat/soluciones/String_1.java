eaa64ddfb1c35b15d9d1f7b6882c9a31e46bed4d
german <germanjongewaard@gmail.com>
2018-03-20 20:08:50 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -312,6 +312,7 @@ public class String_1 {
     
     // Given a string and a second "word" string, we'll say that the word matches the string if it appears at
     // the front of the string, except its first char does not need to match exactly. On a match, return the
+    // front of the string, or otherwise return the empty string. So, so with the string "hippo" the word
     
 }
 
