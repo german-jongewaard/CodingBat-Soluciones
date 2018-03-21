0608c613250e37e4b60e9ec6250bdd3530177444
german <germanjongewaard@gmail.com>
2018-03-21 11:14:41 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -340,6 +340,7 @@ public class String_1 {
         {
             char ch = str.charAt(0);
             StringBuilder stbuild = new StringBuilder(len);
+            if(ch != 'x')
         }
         
         
