95eef103b4ea9dff87112a9f79defa8cdbc0a19e
german <germanjongewaard@gmail.com>
2018-03-21 11:12:41 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -332,7 +332,8 @@ public class String_1 {
     }
     
     // Given a string, if the first or last chars are 'x', return the string without those 'x' chars,
-    // and otherwise return the string unchanged.                 
+    // and otherwise return the string unchanged.   
+    public String withoutX(String str)
                     
                     
 }
