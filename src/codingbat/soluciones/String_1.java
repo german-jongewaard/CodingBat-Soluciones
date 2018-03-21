d20381fefb12019326018e7a35a8dbc11194e169
german <germanjongewaard@gmail.com>
2018-03-21 11:12:14 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -332,7 +332,7 @@ public class String_1 {
     }
     
     // Given a string, if the first or last chars are 'x', return the string without those 'x' chars,
-                    
+    // and otherwise return the string unchanged.                 
                     
                     
 }
