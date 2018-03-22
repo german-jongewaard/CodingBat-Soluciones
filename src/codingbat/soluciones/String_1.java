59443c0a21edf7eea51fd97e28ae383d9a2d3b8d
german <germanjongewaard@gmail.com>
2018-03-22 11:07:10 +0100

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_1.java
+++ src/codingbat/soluciones/String_1.java
@@ -357,7 +357,7 @@ public class String_1 {
     // Given a string, if one or both of the first 2 chars is 'x', return the 
     //string without those 'x' chars
     // and otherwise return the string unchanged. This is a little harder than it looks.
-        
+    public String withoutX2(String str)
         
                     
 }
