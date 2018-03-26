bb162136eb832a4e0d6476477bb5a54fbefda198
german <germanjongewaard@gmail.com>
2018-03-26 11:55:06 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -61,6 +61,13 @@ public class String_2 {
 
     // Return the number of times that the string "code" appears anywhere in the 
     // given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+    public int countCode(String str)
+    {
+        
+        
+        
+        
+        
     
     
 }
