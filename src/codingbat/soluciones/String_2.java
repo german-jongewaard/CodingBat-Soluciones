9a6bea8769709fdc397c898f98ba2873a0822f84
german <germanjongewaard@gmail.com>
2018-03-28 13:16:52 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -142,7 +142,13 @@ public class String_2 {
    // string, there exists a 'y' char somewhere later in the string. 
    // So "xxy" is balanced, but "xyx" is not. One 'y' can balance multiple 
    // 'x's. Return true if the given string is xy-balanced.
-    
+   public boolean xyBalance(String str)
+           
+           
+           
+           
+           
+           
     
     
 }
