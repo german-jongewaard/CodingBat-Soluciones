492e3077fe531866f99668140253b5020c478eb8
german <germanjongewaard@gmail.com>
2018-03-29 13:07:41 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -160,5 +160,7 @@ public class String_2 {
    // Given two strings, A and B, create a bigger string made of the first 
    // char of A, the first char of B, the second char of A, the second char 
    // of B, and so on. Any leftover chars go at the end of the result. 
+   public String mixString(String a, String b)
+   {
     
 }
