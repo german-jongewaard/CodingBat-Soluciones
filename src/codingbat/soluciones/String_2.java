cf988fb45e74af8aa689369c6e1ac040708e3410
german <germanjongewaard@gmail.com>
2018-03-31 00:40:38 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -181,7 +181,8 @@ public class String_2 {
    
    
    // Given a string and an int N, return a string made of N repetitions of the 
-   // last N characters of the string. You may assume that N is between 0 and the length of the string, inclusive.
+   // last N characters of the string. You may assume that N is between 0 and 
+   // the length of the string, inclusive.
    
         
         
