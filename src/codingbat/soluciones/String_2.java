1ee404adc1a55c5f1e8b0da7ac7a9ebace7ac446
german <germanjongewaard@gmail.com>
2018-04-02 00:50:57 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -206,6 +206,8 @@ public class String_2 {
             }
             return stbuild.toString();
     }
+    
+    //Given two strings, word and a separator, return a big string made of count occurences of the word, separated by the separator string. 
 
             
             
