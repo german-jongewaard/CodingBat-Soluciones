8c47a85604c9b0bdca516dba31f0e96756058424
german <germanjongewaard@gmail.com>
2018-04-03 01:00:35 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -226,6 +226,7 @@ public class String_2 {
     // Given a string, consider the prefix string made of the first N chars of 
     // the string. Does that prefix string appear somewhere else in the string? 
     // Assume that the string is not empty and that N is in the range 1..str.length().
+    public boolean prefixAgain(String str, int n)
             
    
         
