546603656dff186546cd17457ecf26e15dd7b589
german <germanjongewaard@gmail.com>
2018-04-03 01:03:18 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -233,7 +233,8 @@ public class String_2 {
     }
             
     // Given a string, does "xyz" appear in the middle of the string? To 
-    // define middle, we'll say that the number of chars to the left and right of the "xyz" must differ by at most one. This problem is harder than it looks.
+    // define middle, we'll say that the number of chars to the left and right 
+    // of the "xyz" must differ by at most one. This problem is harder than it looks.
    
         
         
