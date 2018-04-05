3af6f7ef1373116c4f8bc4fe50ad74373e942415
german <germanjongewaard@gmail.com>
2018-04-05 18:17:25 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -261,7 +261,8 @@ public class String_2 {
     // Return the string that is between the first and last appearance of "bread" 
     // in the given string, or return the empty string "" if there are not two pieces of bread.
     public String getSandwich(String str)                    
-                        
+    {
+            int iFirst = str.indexOf("bread");                    
                         
                         
                         
