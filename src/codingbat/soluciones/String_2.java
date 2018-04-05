6fbe428b3f91cbe784ac820e07778e83d8882d18
german <germanjongewaard@gmail.com>
2018-04-05 18:19:09 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -266,6 +266,7 @@ public class String_2 {
             int iLast = str.lastIndexOf("bread");   
             if(iFirst != -1 && iLast != -1 && iFirst != iLast)  
                       return str.substring(iFirst+5, iLast);
+            return "";
                         
                         
                         
