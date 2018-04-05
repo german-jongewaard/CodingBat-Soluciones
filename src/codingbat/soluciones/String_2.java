d038c58bf2ffd54d4ba5c7f8a99c92b8650f0ed9
german <germanjongewaard@gmail.com>
2018-04-05 18:17:05 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -260,7 +260,7 @@ public class String_2 {
     //A sandwich is two pieces of bread with something in between. 
     // Return the string that is between the first and last appearance of "bread" 
     // in the given string, or return the empty string "" if there are not two pieces of bread.
-                        
+    public String getSandwich(String str)                    
                         
                         
                         
