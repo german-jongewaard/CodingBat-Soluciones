469c6e23c7c0f6a68b5895f41075c912a43778ae
german <germanjongewaard@gmail.com>
2018-04-06 18:25:34 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -283,8 +283,12 @@ public class String_2 {
                      }	
             } 
             return true;
-}
-                            
+    }
+      
+    
+    // Look for patterns like "zip" and "zap" in the string -- length-3, 
+    // starting with 'z' and ending with 'p'. Return a string where for all such 
+    // words, the middle letter is gone, so "zipXzap" yields "zpXzp". 
                             
                             
                             
