096bdeb3e1cdf8d55af5c461819084d27798be5d
german <germanjongewaard@gmail.com>
2018-04-06 18:22:12 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -272,8 +272,7 @@ public class String_2 {
     // Returns true if for every '*' (star) in the string, if there are chars 
     // both immediately before and after the star, they are the same. 
     public boolean sameStarChar(String str)
-                        
-                        
+    {                 
                         
         
 }
