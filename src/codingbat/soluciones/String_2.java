c884be18f8506c9cec663c5cac13e17f6d001740
german <germanjongewaard@gmail.com>
2018-04-08 10:54:22 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -312,6 +312,11 @@ public class String_2 {
             }
             return stbuild.toString(); 
     }
+    
+    
+    // Return a version of the given string, where for every star (*) in the 
+    // string the star and the chars immediately to its left and right are gone. 
+    // So "ab*cd" yields "ad" and "ab**cd" also yields "ad".
                             
                             
                             
