a068c6ce9c00ee91226cfcf27b649c53dfc8e0fc
german <germanjongewaard@gmail.com>
2018-04-09 11:02:04 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_2.java
+++ src/codingbat/soluciones/String_2.java
@@ -330,7 +330,7 @@ public class String_2 {
                             indexes[i] = 1;
                             if(i >= 1)
                                     indexes[i-1] = 1;
-                             
+                            if(i < (len-1)) 
                                     
                      
             
