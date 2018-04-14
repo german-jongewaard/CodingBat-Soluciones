686b895fc7ae307b39f6f681f6d1fdebbe2a8eba
german <germanjongewaard@gmail.com>
2018-04-15 00:33:31 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_3.java
+++ src/codingbat/soluciones/String_3.java
@@ -39,6 +39,8 @@ public class String_3 {
     // where all instances of the remove string have been removed (not case 
     // sensitive). You may assume that the remove string is length 1 or more. 
     // Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+    public String withoutString(String base, String remove)
+    {
                 
             
 }
