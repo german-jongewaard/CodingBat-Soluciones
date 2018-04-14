8f3833ec59e30554ffcaf13c369cba5e0dc5b222
german <germanjongewaard@gmail.com>
2018-04-15 00:32:09 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_3.java
+++ src/codingbat/soluciones/String_3.java
@@ -35,7 +35,8 @@ public class String_3 {
             return count;
     }
     
-    // Given two strings, base and remove, return a version of the base string where all instances of the remove string have been removed (not case sensitive). You may assume that the remove string is length 1 or more. Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+    // Given two strings, base and remove, return a version of the base string 
+    // where all instances of the remove string have been removed (not case sensitive). You may assume that the remove string is length 1 or more. Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
                 
             
 }
