c5234ba07530e6b4c550f4a2ae83833ca8410f83
german <germanjongewaard@gmail.com>
2018-04-15 00:31:43 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_3.java
+++ src/codingbat/soluciones/String_3.java
@@ -33,7 +33,9 @@ public class String_3 {
             if(ch == 'y' || ch == 'z')
                     count++;
             return count;
-                
+    }
+    
+    // Given two strings, base and remove, return a version of the base string where all instances of the remove string have been removed (not case sensitive). You may assume that the remove string is length 1 or more. Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
                 
             
 }
