a0aaf7d5c921566018f9a003c166274bc6a1aead
german <germanjongewaard@gmail.com>
2018-04-16 00:36:32 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_3.java
+++ src/codingbat/soluciones/String_3.java
@@ -44,6 +44,7 @@ public class String_3 {
             int blen = base.length();
             int rlen = remove.length();
             int current = 0;
+            int place;
      
         
         
