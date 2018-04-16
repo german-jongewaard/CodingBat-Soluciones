f3fcd86279252bc7b849c4689a3ae12f7fbce8cb
german <germanjongewaard@gmail.com>
2018-04-17 00:47:24 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_3.java
+++ src/codingbat/soluciones/String_3.java
@@ -66,6 +66,7 @@ public class String_3 {
     }        
                             
     // Given a string, return true if the number of appearances of "is" anywhere
-    // in the string is equal to the number of appearances of "not" anywhere in the string (case sensitive).     
+    // in the string is equal to the number of appearances of "not" anywhere in
+    // the string (case sensitive).     
             
 }
