535e5fd4e723359c0d8a1059bde672717cb1aad3
german <germanjongewaard@gmail.com>
2018-04-18 01:38:59 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_3.java
+++ src/codingbat/soluciones/String_3.java
@@ -88,7 +88,8 @@ public class String_3 {
     }
     
     // We'll say that a lowercase 'g' in a string is "happy" if there is 
-    // another 'g' immediately to its left or right. Return true if all the g's in the given string are happy. 
+    // another 'g' immediately to its left or right. Return true if all the 
+    // g's in the given string are happy. 
     
             
 }
