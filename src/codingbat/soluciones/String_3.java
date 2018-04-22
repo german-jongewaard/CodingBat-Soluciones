2f19ea6d16d92953f4ffdd7ca4e6017bb90e4980
german <germanjongewaard@gmail.com>
2018-04-22 21:54:17 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_3.java
+++ src/codingbat/soluciones/String_3.java
@@ -121,7 +121,8 @@ public class String_3 {
     }            
                                     
     // We'll say that a "triple" in a string is a char appearing three times 
-    // in a row. Return the number of triples in the given string. The triples may overlap.    
+    // in a row. Return the number of triples in the given string. The 
+    // triples may overlap.    
         
             
 }
