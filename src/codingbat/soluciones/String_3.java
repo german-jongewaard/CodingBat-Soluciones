aff038435015227159a3735a5ad0195e53ab7f36
german <germanjongewaard@gmail.com>
2018-04-23 21:55:18 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_3.java
+++ src/codingbat/soluciones/String_3.java
@@ -123,6 +123,7 @@ public class String_3 {
     // We'll say that a "triple" in a string is a char appearing three times 
     // in a row. Return the number of triples in the given string. The 
     // triples may overlap.    
+    public int countTriple(String str)
         
             
 }
