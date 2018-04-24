105913b8dcfc6c8bcca327aa6a3db7fd92c0c7db
german <germanjongewaard@gmail.com>
2018-04-24 22:03:38 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_3.java
+++ src/codingbat/soluciones/String_3.java
@@ -138,5 +138,6 @@ public class String_3 {
        
     // Given a string, return the sum of the digits 0-9 that appear in the 
     // string, ignoring all other characters. Return 0 if there are no digits 
-    // in the string. (Note: Character.isDigit(char) tests if a char is one of the chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
+    // in the string. (Note: Character.isDigit(char) tests if a char is one of 
+    // the chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
 }
