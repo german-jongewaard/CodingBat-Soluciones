f797d60140eb0ed1ca0cdebf07b93be343983dde
german <germanjongewaard@gmail.com>
2018-04-25 22:20:23 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_3.java
+++ src/codingbat/soluciones/String_3.java
@@ -154,7 +154,8 @@ public class String_3 {
             return sum;     
     }
     
-    // Given a string, return the longest substring that appears at both the beginning and end of the string without overlapping. For example, sameEnds("abXab") is "ab".
+    // Given a string, return the longest substring that appears at both the 
+    // beginning and end of the string without overlapping. For example, sameEnds("abXab") is "ab".
     
     
     
