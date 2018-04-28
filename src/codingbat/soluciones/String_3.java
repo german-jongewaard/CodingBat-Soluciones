aa2925673fb95c1b18c558930f61458afaf3b0f7
german <germanjongewaard@gmail.com>
2018-04-28 18:01:37 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_3.java
+++ src/codingbat/soluciones/String_3.java
@@ -181,7 +181,8 @@ public class String_3 {
             return "";
     }
     
-    // Given a string, look for a mirror image (backwards) string at both the beginning and end of the given string. In other words, zero or more characters at the very begining of the given string, and at the very end of the string in reverse order (possibly overlapping). For example, the string "abXYZba" has the mirror end "ab". 
+    // Given a string, look for a mirror image (backwards) string at both the
+    // beginning and end of the given string. In other words, zero or more characters at the very begining of the given string, and at the very end of the string in reverse order (possibly overlapping). For example, the string "abXYZba" has the mirror end "ab". 
     
     
     
