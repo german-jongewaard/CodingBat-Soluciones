07a50562f6d346e5b3620cb6134d362dcf8c317c
german <germanjongewaard@gmail.com>
2018-05-01 18:32:08 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/String_3.java
+++ src/codingbat/soluciones/String_3.java
@@ -225,9 +225,10 @@ public class String_3 {
                 }   
                 if(block > maxBlock)
                                     maxBlock = block;
-                return maxBlock;
-    
+                return maxBlock;    
     }
+    
+    // Given a string, return the sum of the numbers appearing in the string, ignoring all other characters. A number is a series of 1 or more digit chars in a row. (Note: Character.isDigit(char) tests if a char is one of the chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
     
     
     
