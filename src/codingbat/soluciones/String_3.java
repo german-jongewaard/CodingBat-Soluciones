2e6192b25c163aa15aae56ef25a3925a408397f2
german <germanjongewaard@gmail.com>
2018-05-04 22:12:54 +0200

    Implementando CodingBatSolutions

--- src/codingbat/soluciones/String_3.java
+++ src/codingbat/soluciones/String_3.java
@@ -268,6 +268,7 @@ public class String_3 {
     
     // Given a string, return a string where every appearance of the lowercase 
     // word "is" has been replaced with "is not". The word "is" should not be 
-    // immediately preceeded or followed by a letter -- so for example the "is" in "this" does not count. (Note: Character.isLetter(char) tests if a char is a letter.) 
+    // immediately preceeded or followed by a letter -- so for example the 
+    // "is" in "this" does not count. (Note: Character.isLetter(char) tests if a char is a letter.) 
     
 }
