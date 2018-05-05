d9ddd8f064a69039bb4b190945536360b67b2d51
german <germanjongewaard@gmail.com>
2018-05-05 22:21:49 +0200

    Implementando CodingBatSolutions

--- src/codingbat/soluciones/String_3.java
+++ src/codingbat/soluciones/String_3.java
@@ -283,6 +283,7 @@ public class String_3 {
                     if(!Character.isLetter(str.charAt(p-1))) 
                         check++;
                 }    
+                else
                             
     
 }
