7c373a9bda9c3003eb879a6e7f07dd10aab317eb
german <germanjongewaard@gmail.com>
2018-05-06 23:42:01 +0200

    Implementando CodingBatSolutions

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -16,5 +16,5 @@ public class Array_1 {
     
     
     // Given an array of ints, return true if the array is length 1 or more, and the 
-    
+    // first element and the last element are the same. 
 }
