f4b59b685533ba56e2f09bb3c3339346725217f8
german <germanjongewaard@gmail.com>
2018-05-06 23:39:50 +0200

    Implementando CodingBatSolutions

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -8,7 +8,8 @@ package codingbat.soluciones;
 
 public class Array_1 {
     
-    // Given an array of ints, return true if 6 appears as either the first or last element in the array.
-// The array will be length 1 or more. 
+    // Given an array of ints, return true if 6 appears as either the first 
+    // or last element in the array.
+    // The array will be length 1 or more. 
     
 }
