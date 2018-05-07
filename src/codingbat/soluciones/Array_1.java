eb4c0f3200700955a55cbab6c4142765e9a12fc6
german <germanjongewaard@gmail.com>
2018-05-07 23:43:49 +0200

    Implementando CodingBatSolutions

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -21,7 +21,7 @@ public class Array_1 {
     // first element and the last element are the same. 
     public boolean sameFirstLast(int[] nums)
     {         
-        return (nums.length
+        return (nums.length >= 1 &&
     }
             
             
