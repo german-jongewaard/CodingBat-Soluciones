63518a89bc2e587a0c5389564077fdab402550a1
german <germanjongewaard@gmail.com>
2018-05-07 23:44:48 +0200

    Implementando CodingBatSolutions

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -26,7 +26,9 @@ public class Array_1 {
             
     // Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
     public int[] makePi()        
-    {         
+    { 
+        int[] pi = {3, 1, 4}; 
+    }        
             
             
             
