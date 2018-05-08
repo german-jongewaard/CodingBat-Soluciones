6005c82cb127826a20d0a82b07d14cca9cd08815
german <germanjongewaard@gmail.com>
2018-05-08 23:49:59 +0200

    Implementando CodingBatSolutions

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -32,7 +32,7 @@ public class Array_1 {
     
     // Given 2 arrays of ints, a and b, return true if they have the same 
     // first element or they have the same last element.
-// Both arrays will be length 1 or more.
+    // Both arrays will be length 1 or more.
             
             
             
