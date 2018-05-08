476f7cb45ce291961bf4ee551bf7a035f68b2847
german <germanjongewaard@gmail.com>
2018-05-08 23:49:54 +0200

    Implementando CodingBatSolutions

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -30,7 +30,8 @@ public class Array_1 {
         return pi;
     }        
     
-    // Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element.
+    // Given 2 arrays of ints, a and b, return true if they have the same 
+    // first element or they have the same last element.
 // Both arrays will be length 1 or more.
             
             
