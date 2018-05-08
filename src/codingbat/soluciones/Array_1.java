a5da9715bc7216433ef8f0726183ed9822e4d592
german <germanjongewaard@gmail.com>
2018-05-08 23:51:15 +0200

    Implementando CodingBatSolutions

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -34,7 +34,7 @@ public class Array_1 {
     // first element or they have the same last element.
     // Both arrays will be length 1 or more.
     public boolean commonEnd(int[] a, int[] b)
-    {         return (a[0] == b[0] || a[a.length-1]
+    {         return (a[0] == b[0] || a[a.length-1] == b[b.length-1]);
             
             
             
