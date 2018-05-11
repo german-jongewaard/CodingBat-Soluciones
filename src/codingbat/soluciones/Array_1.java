dcada8c3af47b1a39737ad38d9a5d2f710d80d0e
german <germanjongewaard@gmail.com>
2018-05-11 23:58:18 +0200

    Implementando CodingBatSolutions

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -40,7 +40,9 @@ public class Array_1 {
     
     // Given an array of ints length 3, return the sum of all the elements.
     public int sum3(int[] nums)        
-    {         return (nums[0] + nums[1] + nums[2]);
+    {         
+        return (nums[0] + nums[1] + nums[2]);
+    }
             
             
             
