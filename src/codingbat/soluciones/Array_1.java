6cf5c1d56a303f68cbdc7af79a3342188fa880d8
german <germanjongewaard@gmail.com>
2018-05-14 00:10:04 +0200

    Implementando CodingBatSolutions

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -58,6 +58,7 @@ public class Array_1 {
     public int[] reverse3(int[] nums)
     {
         int[] reversed = {nums[2], nums[1], nums[0]};
+        return reversed;
     }
             
             
