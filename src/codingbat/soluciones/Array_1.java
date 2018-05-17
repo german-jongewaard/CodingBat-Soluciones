940068a3628a0d3bddb792def6b6d33c9911567b
german <germanjongewaard@gmail.com>
2018-05-17 03:13:56 +0200

    Implementando Calculadora Java

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -80,6 +80,7 @@ public class Array_1 {
     // Given an array of ints, return the sum of the first 2 elements in the array.
     // If the array length is less than 2, just sum up the elements that exist,
     // returning 0 if the array is length 0.
+    public int sum2(int[] nums)
             
             
 }
