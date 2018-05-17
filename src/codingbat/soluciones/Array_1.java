6be5603b2d3e1362a8c160cc0ce12249dbd7a011
german <germanjongewaard@gmail.com>
2018-05-17 03:14:48 +0200

    Implementando Calculadora Java

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -85,6 +85,7 @@ public class Array_1 {
         if(nums.length >= 2)
                 return (nums[0] + nums[1]);
         if(nums.length == 1)
+                return nums[0];
             
             
 }
