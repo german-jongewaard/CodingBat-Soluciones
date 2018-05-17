26365117acf8a5772033a284eb456db28fcce78c
german <germanjongewaard@gmail.com>
2018-05-17 03:13:47 +0200

    Implementando Calculadora Java

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -76,6 +76,10 @@ public class Array_1 {
             return maxVal;
             
     }
+    
+    // Given an array of ints, return the sum of the first 2 elements in the array.
+    // If the array length is less than 2, just sum up the elements that exist,
+    // returning 0 if the array is length 0.
             
             
 }
