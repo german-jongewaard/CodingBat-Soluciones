e4a3762934637c583c9fd75a126deb1fbf540164
german <germanjongewaard@gmail.com>
2018-05-18 03:17:53 +0200

    Implementando Calculadora Java

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -100,6 +100,8 @@ public class Array_1 {
     // Given an array of ints, return a new array length 2 containing the first and last
     // elements from the original array. The original array will be length 1 or more.
     public int[] makeEnds(int[] nums)
+    {
+        int[] temp = {nums[0], nums[nums.length-1]};
             
             
 }
