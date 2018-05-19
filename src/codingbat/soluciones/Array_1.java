78a72a4c2ffb44d95b592cab588c8274aa835706
german <germanjongewaard@gmail.com>
2018-05-19 03:27:06 +0200

    Implementando Calculadora Java

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -119,9 +119,13 @@ public class Array_1 {
         if(nums[0] == 2 || nums[0] == 3)
                 return false;
         return !(nums[1] == 2 || nums[1] == 3);
-        
-        
     }        
+    
+    // Given an int array, return a new array with double the length where its 
+    // last element is the same as
+    // the original array, and all the other elements are 0. The original 
+    // array will be length 1 or more.
+    // Note: by default, a new int array contains all 0's.
             
             
 }
