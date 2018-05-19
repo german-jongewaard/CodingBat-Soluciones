372abf490d5dfc08e4d7107521a9bd6f9478ddd8
german <germanjongewaard@gmail.com>
2018-05-19 03:20:38 +0200

    Implementando Calculadora Java

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -108,7 +108,7 @@ public class Array_1 {
     // Given an int array length 2, return true if it contains a 2 or a 3.
     public boolean has23(int[] nums)
     {
-        if(nums[0]   
+        if(nums[0] == 2 || nums[0] == 3)         
             
             
 }
