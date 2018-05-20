14dc3de97e67bac544a32daaa1a4eef2ee59c0fa
german <germanjongewaard@gmail.com>
2018-05-20 03:29:45 +0200

    Implementando Calculadora Java

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -137,5 +137,7 @@ public class Array_1 {
     // Given an int array, return true if the array contains 2 twice, or 3 twice.
     // The array will be length 0, 1, or 2.
     public boolean double23(int[] nums)
+    {
+            if(nums.length == 2)        
             
 }
