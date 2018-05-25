9b7965742edbc6a8745847ac1a8ea8d511858d2a
german <germanjongewaard@gmail.com>
2018-05-25 03:35:55 +0200

    Implementando Calculadora Java

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -150,5 +150,6 @@ public class Array_1 {
     // Given an int array length 3, if there is a 2 in the array 
     // immediately followed by a 3,
     // set the 3 element to 0. Return the changed array.
+    public int[] fix23(int[] nums)
             
 }
