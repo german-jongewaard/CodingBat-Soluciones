a037bb07d8c1dc06d0f41a90f0c62d5a6983cef2
german <germanjongewaard@gmail.com>
2018-05-25 03:35:43 +0200

    Implementando Calculadora Java

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -145,6 +145,10 @@ public class Array_1 {
                 return (nums[0] == 3 && nums[1] == 3);
             }    
             return false;
-    }        
+    }
+    
+    // Given an int array length 3, if there is a 2 in the array 
+    // immediately followed by a 3,
+    // set the 3 element to 0. Return the changed array.
             
 }
