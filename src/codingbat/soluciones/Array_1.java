6c4ca9a6fa98652af49e6abaddea5010cabc70a5
german <germanjongewaard@gmail.com>
2018-05-25 03:36:37 +0200

    Implementando Calculadora Java

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -155,5 +155,6 @@ public class Array_1 {
             int[] fxArr = {nums[0], nums[1], nums[2]};        
                     if(nums[0] == 2 && nums[1] == 3)
                                     fxArr[1] = 0;
+                    if(nums[1] == 2 && nums[2] == 3)
     }
 }
