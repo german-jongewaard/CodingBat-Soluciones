178936a292b63601c75a55716aa0858e97d768e3
german <germanjongewaard@gmail.com>
2018-06-05 22:42:47 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -164,3 +164,5 @@ public class Array_1 {
 // Start with 2 int arrays, a and b, of any length. 
 // Return how many of the arrays have 1 as their first element.
 public int start1(int[] a, int[] b)
+{
+    int ones = 0;
