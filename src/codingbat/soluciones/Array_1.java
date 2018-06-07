67ab95d656b3327b49ba24e41b41ab6307951909
german <germanjongewaard@gmail.com>
2018-06-07 22:50:04 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -188,5 +188,6 @@ public class Array_1 {
     
     // Given an array of ints of even length, return a new 
     // array length 2 containing the middle
+    // two elements from the original array. The original array will be length 2 or more.
 
 }
