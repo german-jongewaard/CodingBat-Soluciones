4b3c8aebed011f512081e7d17b2d638147902e71
german <germanjongewaard@gmail.com>
2018-06-10 23:04:49 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -232,5 +232,6 @@ public class Array_1 {
     
     // Given an array of ints of odd length, look at the first, last, and middle values in
     // the array and return the largest. The array length will be a least 1. 
+    public int maxTriple(int[] nums)
 
 }
