e143b60414ac7b2de51257135c307dc2a5cd37fd
german <germanjongewaard@gmail.com>
2018-06-10 23:05:45 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -238,5 +238,6 @@ public class Array_1 {
         if(max <= nums[nums.length-1])
             max = nums[nums.length-1];
         if(max <= nums[nums.length/2])
+            max = nums[nums.length/2];
 
 }
