e64ecbe11ee2e5f60a21deb53028524933a5ddf3
german <germanjongewaard@gmail.com>
2018-06-11 23:08:08 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -245,5 +245,7 @@ public class Array_1 {
 
     // Given an int array of any length, return a new array of its first 2 elements.    
     // If the array is smaller than length 2, use whatever elements are present.
+    public int[] frontPiece(int[] nums)
+    {
 
 }
