94484ba50cb9b8935e38e249e1dfa7d3663a3d5f
german <germanjongewaard@gmail.com>
2018-06-11 23:30:42 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -265,6 +265,7 @@ public class Array_1 {
     }
     
     // We'll say that a 1 immediately followed by a 3 in an array is an "unlucky" 1.
-    // Return true if the given array contains an unlucky 1 in the first 2 or last 2 positions in the array.
+    // Return true if the given array contains an unlucky 1 in the first 2 or 
+    // last 2 positions in the array.
 
 }
