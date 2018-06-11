2ff643ccf48c2a84f4db02600e47e940d2a23e28
german <germanjongewaard@gmail.com>
2018-06-11 23:32:04 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -275,6 +275,8 @@ public class Array_1 {
             if((nums[0] == 1 && nums[1] == 3) || (nums[1] == 1 && nums[2] == 3))   
                 return true;
             return (nums[lastP-1] == 1 && nums[lastP] == 3);
+        }
+            if(lastP == 1)    
     }
 
 }
