782e98765b18ef9ff846f8767b391cde814ac27b
german <germanjongewaard@gmail.com>
2018-06-12 23:34:26 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_1.java
+++ src/codingbat/soluciones/Array_1.java
@@ -284,5 +284,6 @@ public class Array_1 {
     // Given 2 int arrays, a and b, return a new array length 2 containing, 
     // as much as will fit,
     // the elements from a followed by the elements from b.
+    // The arrays may be any length, including 0, but there will be 2 or more elements available between the 2 arrays.
 
 }
