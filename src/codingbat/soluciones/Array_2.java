b720bc1aef31bc691358f943eeea75476958f32a
german <germanjongewaard@gmail.com>
2018-06-21 14:08:38 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -7,5 +7,6 @@ package codingbat.soluciones;
  */
 public class Array_2 {
     
-    // Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+    // Return the number of even ints in the given array. Note: the % "mod" 
+    // operator computes the remainder, e.g. 5 % 2 is 1.
 }
