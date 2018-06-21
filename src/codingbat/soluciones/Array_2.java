daf63838ae1e64a7445996ae33fe495d22d28401
german <germanjongewaard@gmail.com>
2018-06-21 14:09:01 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -10,4 +10,4 @@ public class Array_2 {
     // Return the number of even ints in the given array. Note: the % "mod" 
     // operator computes the remainder, e.g. 5 % 2 is 1.
     public int countEvens(int[] nums)
-}
+}   {
