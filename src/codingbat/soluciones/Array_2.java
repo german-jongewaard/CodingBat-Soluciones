f10b2f4b4e048a068b8ebebc88aae9e31171f94f
german <germanjongewaard@gmail.com>
2018-06-22 14:10:54 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -15,6 +15,7 @@ public class Array_2 {
         for(int i = 0; i < nums.length; i++)
         {   
             if(nums[i] % 2 == 0)
+                count++;
         }    
             
             
