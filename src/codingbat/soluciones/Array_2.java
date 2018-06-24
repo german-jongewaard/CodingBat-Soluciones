3a680a8700873ccaa1361f9a406e02a2cd685ec7
german <germanjongewaard@gmail.com>
2018-06-24 14:20:11 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -56,7 +56,9 @@ public class Array_2 {
                         min = nums[i];
             }    
             return (sum-max-min) / (nums.length - 2);
-    }        
+    }
+    
+    // Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
     
 }   
  
