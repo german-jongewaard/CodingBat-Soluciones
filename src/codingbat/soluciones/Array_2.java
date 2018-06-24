5023a4aac72d2e8fb953f103bae50273257f20f2
german <germanjongewaard@gmail.com>
2018-06-24 14:19:32 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -53,6 +53,7 @@ public class Array_2 {
                     if(nums[i] > max)
                         max = nums[i];
                     else if(nums[i] < min)
+                        min = nums[i];
             }    
     
     
