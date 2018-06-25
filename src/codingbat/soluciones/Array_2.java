169dc4dd5add31da13f7d1f8a4b1e421de80dc54
german <germanjongewaard@gmail.com>
2018-06-25 14:21:26 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -61,6 +61,7 @@ public class Array_2 {
     // Return the sum of the numbers in the array, returning 0 for an empty array. 
     // Except the number 13 is very unlucky, so it does not count and numbers 
     // that come immediately after a 13 also do not count.
+    public int sum13(int[] nums) 
     
 }   
  
