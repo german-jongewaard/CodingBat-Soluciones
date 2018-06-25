7270895a6b6e1e749f96dc5dac85c2b14622660d
german <germanjongewaard@gmail.com>
2018-06-25 14:23:25 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -74,6 +74,7 @@ public class Array_2 {
 	return sum;
     }    
     
-    // Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers. 
+    // Return the sum of the numbers in the array, except ignore sections of 
+    // numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers. 
 }   
  
