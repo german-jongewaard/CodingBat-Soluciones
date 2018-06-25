c6ece6b51a7f9fc4321b42129c8e91f3ef8fe454
german <germanjongewaard@gmail.com>
2018-06-26 01:23:45 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -77,6 +77,8 @@ public class Array_2 {
     // Return the sum of the numbers in the array, except ignore sections of 
     // numbers starting with a 6 and extending to the next 7 (every 6 will be 
     // followed by at least one 7). Return 0 for no numbers. 
-    public int sum67(int[] nums)
+    public int sum67(int[] nums){
+            
+            
 }   
  
