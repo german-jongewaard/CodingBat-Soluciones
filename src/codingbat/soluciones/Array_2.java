9ca5aee2e5a11abcbff658b79191e623ead13767
german <germanjongewaard@gmail.com>
2018-06-27 01:34:32 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -102,6 +102,8 @@ public class Array_2 {
     public boolean has22(int[] nums)
     {
         for(int i = 0; i < nums.length-1; i++)
+        {
+                if(nums[i] == 2 && nums[i+1] == 2)
     }
     
     
