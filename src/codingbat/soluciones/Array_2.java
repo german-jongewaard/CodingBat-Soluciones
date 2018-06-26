747325d27f4ad73d989e293af8761eb38b9f412f
german <germanjongewaard@gmail.com>
2018-06-27 01:29:45 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -101,7 +101,7 @@ public class Array_2 {
     // a 2 next to a 2 somewhere.
     public boolean has22(int[] nums)
     {
-        
+        for(int i = 0; i < nums.length-1; i++)
     }
     
     
