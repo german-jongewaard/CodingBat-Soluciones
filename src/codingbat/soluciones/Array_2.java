3397cbbdcaad7171e740c363799549e70356b941
german <germanjongewaard@gmail.com>
2018-06-28 01:38:54 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -112,6 +112,8 @@ public class Array_2 {
     
     // Given an array of ints, return true if the array contains no 1's and no 3's.
     public boolean lucky13(int[] nums)
+    {
+                for(int i = 0; i < nums.length; i++)        
     
     
     
