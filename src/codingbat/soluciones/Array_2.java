5ac9cebb4ce9012acfc8a28c531286b1251798c8
german <germanjongewaard@gmail.com>
2018-06-28 01:40:48 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -120,7 +120,8 @@ public class Array_2 {
             }
             return true;        
     }        
-                    
+                 
+    // Given an array of ints, return true if the sum of all the 2's in the array is exactly 8.
     
     
     
