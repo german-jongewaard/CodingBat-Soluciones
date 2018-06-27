bd32c9437bfea88592448dbf97f9419e03a5ef27
german <germanjongewaard@gmail.com>
2018-06-28 01:39:27 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -115,7 +115,8 @@ public class Array_2 {
     {
             for(int i = 0; i < nums.length; i++)        
             {
-                    if(nums[i] == 1 || nums[i] == 3)        
+                    if(nums[i] == 1 || nums[i] == 3)  
+                                return false;
                     
     
     
