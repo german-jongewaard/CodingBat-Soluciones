706bba278855561100dc63a5d3ecff1ea6cf5fdc
german <germanjongewaard@gmail.com>
2018-06-30 02:09:21 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -151,7 +151,8 @@ public class Array_2 {
     
     // Given an array of ints, return true if every element is a 1 or a 4.
     public boolean only14(int[] nums)
-    
+    {
+            for(int i = 0; i < nums.length; i++)
     
     
     
