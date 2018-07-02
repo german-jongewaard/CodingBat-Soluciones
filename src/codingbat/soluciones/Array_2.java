2d75ac33444c541d9ddd09517a276208d8557905
german <germanjongewaard@gmail.com>
2018-07-02 02:16:44 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -176,7 +176,8 @@ public class Array_2 {
     }
     
     // We'll say that a value is "everywhere" in an array if for every 
-    // pair of adjacent elements in the array, at least one of the pair is that value. Return true if the given value is everywhere in the array.
+    // pair of adjacent elements in the array, at least one of the pair 
+    // is that value. Return true if the given value is everywhere in the array.
     
 }   
  
