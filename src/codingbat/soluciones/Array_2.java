1598c9e9e03577658313d898ea82c91cb64cfc98
german <germanjongewaard@gmail.com>
2018-07-02 02:29:35 +0200

    Implementando Soluciones de CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -178,7 +178,7 @@ public class Array_2 {
     // We'll say that a value is "everywhere" in an array if for every 
     // pair of adjacent elements in the array, at least one of the pair 
     // is that value. Return true if the given value is everywhere in the array.
-    
+    public boolean isEverywhere(int[] nums, int val)
     
     
     
