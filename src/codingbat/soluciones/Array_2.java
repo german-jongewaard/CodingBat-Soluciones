f3ce23d1e8acfbbd669d8c5726db9d8517483e7c
german <germanjongewaard@gmail.com>
2018-07-16 04:21:06 +0200

    Soluciones ejercicios

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -191,5 +191,6 @@ public class Array_2 {
     
     // Given an array of ints, return true if the array contains a 2 next 
     // to a 2 or a 4 next to a 4, but not both.
+    public boolean either24(int[] nums)
 }   
  
