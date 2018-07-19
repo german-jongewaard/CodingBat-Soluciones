c67e393f35555c7ce3877fca9c3d13bc521036ad
german <germanjongewaard@gmail.com>
2018-07-19 04:28:41 +0200

    Soluciones ejercicios

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -230,9 +230,10 @@ public class Array_2 {
                 }
 	}
         return false;
-        }
-            
+        } 
     }
+
+// Given an array of ints, return true if there is a 1 in the array with a 2 somewhere later in the array. 
     
     
     
