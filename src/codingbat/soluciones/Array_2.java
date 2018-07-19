821f4836692af7bbc2dee9cfe37850196cfd3fbf
german <germanjongewaard@gmail.com>
2018-07-19 04:23:42 +0200

    Soluciones ejercicios

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -202,5 +202,6 @@ public class Array_2 {
             }
         return ((no2pair ^ no4pair) == 1);
     }
+    // Given arrays nums1 and nums2 of the same length, for every element in nums1, consider the corresponding element in nums2 (at the same index). Return the count of the number of times that the two elements differ by 2 or less, but are not equal. 
 }   
  
