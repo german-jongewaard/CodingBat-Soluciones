05a8fa0325b54a7a5e680841803a0ebb0b7ce1cc
german <germanjongewaard@gmail.com>
2018-07-19 04:24:14 +0200

    Soluciones ejercicios

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -203,7 +203,9 @@ public class Array_2 {
         return ((no2pair ^ no4pair) == 1);
     }
     // Given arrays nums1 and nums2 of the same length, for every element in nums1, 
-    // consider the corresponding element in nums2 (at the same index). Return the count of the number of times that the two elements differ by 2 or less, but are not equal. 
+    // consider the corresponding element in nums2 (at the same index). Return the 
+    //count of the number of times that the two elements differ by 2 or less, but are not equal. 
+    public int matchUp(int[] nums1, int[] nums2)
     
     
     
