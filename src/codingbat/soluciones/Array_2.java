4abff1054d94f8a2dbc3abc2d22cd5ad2bab8893
german <germanjongewaard@gmail.com>
2018-07-19 04:24:59 +0200

    Soluciones ejercicios

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -208,7 +208,9 @@ public class Array_2 {
     public int matchUp(int[] nums1, int[] nums2){
         int count = 0;
         int diff;
-        for(int i = 0; i < nums1.length; i++)
+        for(int i = 0; i < nums1.length; i++){
+            diff = Math.abs(nums1[i] - nums2[i]);
+        }
     }
     
     
