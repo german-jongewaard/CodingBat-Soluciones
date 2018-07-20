067b71b80a9843d3af876a35b300bc0488eff0fe
german <germanjongewaard@gmail.com>
2018-07-20 04:26:14 +0200

    Soluciones ejercicios

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -217,7 +217,8 @@ public class Array_2 {
     }
     
     // Given an array of ints, return true if the array contains two 7's next 
-    // to each other, or there are two 7's separated by one element, such as with {7, 1, 7}.
+    // to each other, or there are two 7's separated by one element, 
+    // such as with {7, 1, 7}.
     
     
     
