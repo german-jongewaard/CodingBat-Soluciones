6ddc6b77c7e44b779b00b74283db1a528892a1c5
german <germanjongewaard@gmail.com>
2018-07-21 04:36:01 +0200

    Soluciones ejercicios

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -231,28 +231,26 @@ public class Array_2 {
 	}
         return false;
         } 
-    }
+   
 
     // Given an array of ints, return true if there is a 1 in the array with a 
     // 2 somewhere later in the array. 
-    public boolean has12(int[] nums){
-        
-        boolean oneSpotted = false;
-        for(int i = 0; i < nums.length; i++){
-            
-            if(oneSpotted){
-                
-                if(nums[i] == 2)
-                    return true;
-
-                } else if(nums[i] == 1)
+    public boolean has12(int[] nums)
+    {
+      boolean oneSpotted = false;
+      for(int i = 0; i < nums.length; i++)
+      {
+            if(oneSpotted)
+            {
+                    if(nums[i] == 2)
+                            return true;
+            }
+            else if(nums[i] == 1)
                     oneSpotted = true;
-
-        }
+      }
+      return false;
     }
     
     
-    
-    
-}   
- 
+
+}
