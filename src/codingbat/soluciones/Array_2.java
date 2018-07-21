e578b378f9dd5f4d9dcad47d1851562eb26a95bf
german <germanjongewaard@gmail.com>
2018-07-21 04:36:05 +0200

    Soluciones ejercicios

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -251,6 +251,6 @@ public class Array_2 {
       return false;
     }
     
-    
+    // Given an array of ints, return true if the array contains either 3 even or 3 odd values all next to each other.
 
 }
