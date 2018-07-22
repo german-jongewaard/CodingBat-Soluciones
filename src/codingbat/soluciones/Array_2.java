a414a709d6acbb331f3da62db49d10b61280c185
german <germanjongewaard@gmail.com>
2018-07-22 04:37:08 +0200

    Soluciones ejercicios

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -256,7 +256,7 @@ public class Array_2 {
     public boolean modThree(int[] nums){
         for(int i = 0; i < nums.length - 2; i++){
             if(nums[i] % 2 == nums[i+1] % 2 && nums[i] % 2 == nums[i+2] % 2)
-                
+                return true;
         }
     }
 
