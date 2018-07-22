d2a0d9b4c41beb46a12d7a393a408fd4c19278ac
german <germanjongewaard@gmail.com>
2018-07-22 04:38:02 +0200

    Soluciones ejercicios

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -264,7 +264,10 @@ public class Array_2 {
     
     // Given an array of ints, return true if the value 3 appears in the array 
     // exactly 3 times, and no 3's are next to each other.
-    public boolean haveThree(int[] nums)
+    public boolean haveThree(int[] nums){
+        int count = 0;
+        
+    }
     
     
     
