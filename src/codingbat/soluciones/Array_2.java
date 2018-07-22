8948ec6c83b4a33387f9ed172843f8a05c336ed3
german <germanjongewaard@gmail.com>
2018-07-22 04:36:26 +0200

    Soluciones ejercicios

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -253,5 +253,6 @@ public class Array_2 {
     
     // Given an array of ints, return true if the array contains either 3 even
     // or 3 odd values all next to each other.
+    public boolean modThree(int[] nums)
 
 }
