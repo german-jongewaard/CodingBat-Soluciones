78dda20cc12c215b2379a35e6c887c365f953ca0
german <germanjongewaard@gmail.com>
2018-07-24 01:20:52 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -303,7 +303,8 @@ public class Array_2 {
     //are the same. For example, with {5, 6, 45, 99, 13, 5, 6}, the ends are 
     //the same for n=0 and n=2, and false for n=1 and n=3. You may assume 
     //that n is in the range 0..nums.length inclusive. 
-    
+    public boolean sameEnds(int[] nums, int len)
+    {
     
     
 }
