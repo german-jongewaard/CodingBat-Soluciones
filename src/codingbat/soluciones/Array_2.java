bb0f0312d30941dd9dfabd9a1b7907f85bd7c013
german <germanjongewaard@gmail.com>
2018-07-23 04:41:18 +0200

    Soluciones ejercicios

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -284,7 +284,9 @@ public class Array_2 {
     
     // Given an array of ints, return true if every 2 that appears in 
     // the array is next to another 2.
-    
+    public boolean twoTwo(int[] nums){
+        
+    }
     
     
     
