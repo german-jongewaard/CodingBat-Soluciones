8dd1ed17d893ed9dd5156fc64cfd600b381ddaed
german <germanjongewaard@gmail.com>
2018-07-25 01:24:39 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -324,7 +324,8 @@ public class Array_2 {
     // Return true if the array contains, somewhere, three increasing adjacent
     // numbers like .... 4, 5, 6, ... or 23, 24, 25. 
     public boolean tripleUp(int[] nums)
-    
+    {
+        boolean prevUp = false;
     
     
 }
