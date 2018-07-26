0c5cb5c9ac85dce02724b6418679c6e80d1b0977
german <germanjongewaard@gmail.com>
2018-07-27 01:34:27 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -357,7 +357,8 @@ public class Array_2 {
     }
     
     // For each multiple of 10 in the given array, change all the values 
-    //following it to be that multiple of 10, until encountering another multiple of 10. So {2, 10, 3, 4, 20, 5} yields {2, 10, 10, 10, 20, 20}.
+    //following it to be that multiple of 10, until encountering another 
+    //multiple of 10. So {2, 10, 3, 4, 20, 5} yields {2, 10, 10, 10, 20, 20}.
     
     
     
