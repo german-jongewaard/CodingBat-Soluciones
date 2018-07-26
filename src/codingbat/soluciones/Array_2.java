ec37d87e8b7b55e468b10567d37190e599858453
german <germanjongewaard@gmail.com>
2018-07-27 01:33:39 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -351,6 +351,7 @@ public class Array_2 {
             int temp = nums[0];
             for(int i = 0; i < nums.length - 1; i++)
                 nums[i] = nums[i+1];
+            nums[nums.length-1] = temp;
     }
     
     
