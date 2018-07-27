35c1cd614a09034bf7726df3e85a12991c968a2d
german <germanjongewaard@gmail.com>
2018-07-28 01:39:28 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -374,7 +374,8 @@ public class Array_2 {
     
     // Given a non-empty array of ints, return a new array containing the 
     //elements from the original array that come before the first 4 in the 
-    //original array. The original array will contain at least one 4. Note that it is valid in java to create an array of length 0.
+    //original array. The original array will contain at least one 4. Note 
+    //that it is valid in java to create an array of length 0.
     
     
     
