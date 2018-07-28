0844370b0b6b90d9827dd82e65f3951f6a88fb6c
german <germanjongewaard@gmail.com>
2018-07-29 01:39:45 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -376,7 +376,8 @@ public class Array_2 {
     //elements from the original array that come before the first 4 in the 
     //original array. The original array will contain at least one 4. Note 
     //that it is valid in java to create an array of length 0.
-    
+    public int[] pre4(int[] nums)
+            
     
     
     
