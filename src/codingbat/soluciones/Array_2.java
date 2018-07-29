693cd8382ff12e7f4c36249472739a4a55ad93d2
german <germanjongewaard@gmail.com>
2018-07-30 01:45:45 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -392,7 +392,7 @@ public class Array_2 {
     //elements from the original array that come after the last 4 in the 
     //original array. The original array will contain at least one 4. Note 
     //that it is valid in java to create an array of length 0.
-    
+    public int[] post4(int[] nums)
     
     
     
