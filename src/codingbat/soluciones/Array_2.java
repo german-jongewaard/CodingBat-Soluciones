b612d03fabd443a9dad9b9bda43b475351f197d0
german <germanjongewaard@gmail.com>
2018-07-31 01:47:01 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -400,6 +400,7 @@ public class Array_2 {
              p--;
          arr = new int[nums.length - 1 - p];
          for(int i = p + 1; i < nums.length; i++)
+             arr[i - p - 1] = nums[i];
     
     
     
