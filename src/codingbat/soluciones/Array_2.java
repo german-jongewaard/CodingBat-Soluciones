e285318975dfbb33888044b27cada4a5c9e1ddaf
german <germanjongewaard@gmail.com>
2018-07-31 01:48:02 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -406,7 +406,8 @@ public class Array_2 {
     
     // We'll say that an element in an array is "alone" if there are values 
     //before and after it, and those values are different from it. Return a 
-    //version of the given array where every instance of the given value which is alone is replaced by whichever value to its left or right is larger.
+    //version of the given array where every instance of the given value which
+    is alone is replaced by whichever value to its left or right is larger.
     
     
     
