68cc56636f475c9531cb68be7b3d637a24cc8ca3
german <germanjongewaard@gmail.com>
2018-08-01 02:22:26 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -408,7 +408,7 @@ public class Array_2 {
     //before and after it, and those values are different from it. Return a 
     //version of the given array where every instance of the given value which
     //is alone is replaced by whichever value to its left or right is larger.
-    
+    public int[] notAlone(int[] nums, int val)
     
     
     
