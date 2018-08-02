f3ac335151848827c5ffd5fdc532a1619112952f
german <germanjongewaard@gmail.com>
2018-08-02 02:33:39 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -425,7 +425,7 @@ public class Array_2 {
     //array, but rearranged so that all the zeros are grouped at the start of 
     //the array. The order of the non-zero numbers does not matter. 
     //So {1, 0, 0, 1} becomes {0 ,0, 1, 1}. You may modify and return the 
-    given array or make a new array.
+    //given array or make a new array.
     
     
     
