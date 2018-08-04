e2206ef6a06a1c2177fb66c98b6b43527755a0c2
german <germanjongewaard@gmail.com>
2018-08-04 02:38:02 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -453,7 +453,8 @@ public class Array_2 {
         for(int i = 0; i < nums.length; i++)
         {
             if(nums[i] != 10)
-                
+            {
+                arr[p] = nums[i];
 
 
 
