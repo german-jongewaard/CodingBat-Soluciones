b023758082bef0f90182f30cedfe0e5201b8b5c1
german <germanjongewaard@gmail.com>
2018-08-05 02:43:32 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -462,7 +462,8 @@ public class Array_2 {
         return arr;
     }    
 
-    // Return a version of the given array where each zero value in the array is replaced by the largest odd value to the right of the zero in the array. If there is no odd value to the right of the zero, leave the zero as a zero.    
+    // Return a version of the given array where each zero value in the array 
+    is replaced by the largest odd value to the right of the zero in the array. If there is no odd value to the right of the zero, leave the zero as a zero.    
 
 
     
