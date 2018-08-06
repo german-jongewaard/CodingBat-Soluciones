40d0820cd1ab0ed069cb72f8c873bf4ac52ebae6
german <germanjongewaard@gmail.com>
2018-08-06 02:45:47 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -478,7 +478,8 @@ public class Array_2 {
                 {
                     if(nums[k] > max && nums[k] % 2 == 1)
                         max = nums[k];
-                
+                }
+if(max != 0)
             
             
             
