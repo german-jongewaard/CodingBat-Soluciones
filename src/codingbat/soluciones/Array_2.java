a1e96e1861ff94eb7a79dd8e0229bf640af20e3d
german <germanjongewaard@gmail.com>
2018-08-08 02:47:33 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -489,7 +489,8 @@ public class Array_2 {
     // Return an array that contains the exact same numbers as the given 
     //array, but rearranged so that all the even numbers come before all 
     //the odd numbers. Other than that, the numbers can be in any order. 
-    //You may modify and return the given array, or make a new array.        
+    //You may modify and return the given array, or make a new array.
+    public int[] evenOdd(int[] nums)
             
             
     
