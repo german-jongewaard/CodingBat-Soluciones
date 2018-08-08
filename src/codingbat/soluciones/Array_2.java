c6ecf1369d232c6e05dc6acdfd4ee6140211b228
german <germanjongewaard@gmail.com>
2018-08-08 02:48:27 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_2.java
+++ src/codingbat/soluciones/Array_2.java
@@ -496,7 +496,7 @@ public class Array_2 {
         int evenIndex = 0;
         for(int i = 0; i < nums.length; i++)
         {
-            
+            if(nums[i] % 2 == 0)
         
         
         
