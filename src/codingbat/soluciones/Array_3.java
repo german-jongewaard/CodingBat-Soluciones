bf6bbc253be2b083a77cccf257dba34e56704063
german <germanjongewaard@gmail.com>
2018-08-09 02:52:19 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_3.java
+++ src/codingbat/soluciones/Array_3.java
@@ -6,6 +6,7 @@ package codingbat.soluciones;
  */
 public class Array_3 {
 
-    // Consider the leftmost and righmost appearances of some value in an array. We'll say that the "span" is the number of elements between the two inclusive. A single value has a span of 1. Returns the largest span found in the given array. (Efficiency is not a priority.)
+    // Consider the leftmost and righmost appearances of some value in an 
+    array. We'll say that the "span" is the number of elements between the two inclusive. A single value has a span of 1. Returns the largest span found in the given array. (Efficiency is not a priority.)
     
 }
