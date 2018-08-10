afa746c95d980a82709950b81dee31cdc5ff508c
german <germanjongewaard@gmail.com>
2018-08-10 02:55:05 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_3.java
+++ src/codingbat/soluciones/Array_3.java
@@ -20,6 +20,7 @@ public class Array_3 {
             for(j = nums.length - 1; nums[i] != nums[j]; j--);
                 span = 1 + j - i;
                 if(span > maxSpan)
+                    maxSpan = span;
     
     
     
