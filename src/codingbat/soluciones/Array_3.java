8e7c0bd369b352f851ed4607fd0a06a832fa5e49
german <germanjongewaard@gmail.com>
2018-08-14 02:59:41 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_3.java
+++ src/codingbat/soluciones/Array_3.java
@@ -49,7 +49,7 @@ public class Array_3 {
     //but rearranged so that every 4 is immediately followed by a 5. Do not 
     //move the 4's, but every other number may move. The array contains the 
     //same number of 4's and 5's, and every 4 has a number after it that is 
-    not a 4. In this version, 5's may appear anywhere in the original array.    
+    //not a 4. In this version, 5's may appear anywhere in the original array.    
     
     
     
