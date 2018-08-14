5da4fe6823762a3ba77eced5851db5109d221da5
german <germanjongewaard@gmail.com>
2018-08-14 03:01:19 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_3.java
+++ src/codingbat/soluciones/Array_3.java
@@ -55,11 +55,11 @@ public class Array_3 {
          int j = 0;
          for(int i = 0; i < nums.length - 1; i++)
          {
-             if(nums[i] == 4 && nums[i+1] != 5)
-             {
-                 for(; !(nums[j] == 5 && (j == 0 || j > 0 && nums[j-1] != 4)); j++);
-                        nums[j] = nums[i+1];
-                        nums[i+1] = 5;
+            if(nums[i] == 4 && nums[i+1] != 5)
+            {
+                for(; !(nums[j] == 5 && (j == 0 || j > 0 && nums[j-1] != 4)); j++);
+                       nums[j] = nums[i+1];
+                       nums[i+1] = 5;
             
             
             
