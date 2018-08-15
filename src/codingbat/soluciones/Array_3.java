ca703c8eacaf5f809737377017deed52aab98ec6
german <germanjongewaard@gmail.com>
2018-08-15 03:02:11 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_3.java
+++ src/codingbat/soluciones/Array_3.java
@@ -52,17 +52,17 @@ public class Array_3 {
     //not a 4. In this version, 5's may appear anywhere in the original array.    
     public int[] fix45(int[] nums)
     { 
-         int j = 0;
-         for(int i = 0; i < nums.length - 1; i++)
-         {
+        int j = 0;
+        for(int i = 0; i < nums.length - 1; i++)
+        {
             if(nums[i] == 4 && nums[i+1] != 5)
             {
                 for(; !(nums[j] == 5 && (j == 0 || j > 0 && nums[j-1] != 4)); j++);
                        nums[j] = nums[i+1];
                        nums[i+1] = 5;
             }
-         }    
-         return nums;
+        }    
+        return nums;
     }     
             
             
