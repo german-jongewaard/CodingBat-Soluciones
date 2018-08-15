c65386faeda551ab79fbaa8a5becdce094f04aa9
german <germanjongewaard@gmail.com>
2018-08-15 03:04:40 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_3.java
+++ src/codingbat/soluciones/Array_3.java
@@ -66,7 +66,8 @@ public class Array_3 {
     }     
     
     // Given a non-empty array, return true if there is a place to split the 
-    //array so that the sum of the numbers on one side is equal to the sum of the numbers on the other side.
+    //array so that the sum of the numbers on one side is equal to the sum of 
+    the numbers on the other side.
             
             
             
