732db9dbb341909824db1d90e305bbe70faf71ac
german <germanjongewaard@gmail.com>
2018-08-16 03:05:55 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_3.java
+++ src/codingbat/soluciones/Array_3.java
@@ -74,6 +74,7 @@ public class Array_3 {
         int right;    
         for(int i = 0; i < nums.length - 1; i++)
                 left += nums[i];
+                right = nums[nums.length-1];
             
             
             
