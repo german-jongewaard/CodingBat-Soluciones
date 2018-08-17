7a7efed5cede3eb9c04d222a2d90b178dc4763eb
german <germanjongewaard@gmail.com>
2018-08-17 03:07:52 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_3.java
+++ src/codingbat/soluciones/Array_3.java
@@ -86,7 +86,7 @@ public class Array_3 {
     }        
             
     // Given two arrays of ints sorted in increasing order, outer and inner, 
-//return true if all of the numbers in inner appear in outer. The best solution makes only a single "linear" pass of both arrays, taking advantage of the fact that both arrays are already in sorted order.
+    //return true if all of the numbers in inner appear in outer. The best solution makes only a single "linear" pass of both arrays, taking advantage of the fact that both arrays are already in sorted order.
             
     
     
