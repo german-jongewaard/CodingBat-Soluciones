0f7811ef14a3b88e56e90c2aa8e39171f79deda6
german <germanjongewaard@gmail.com>
2018-08-19 00:19:33 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_3.java
+++ src/codingbat/soluciones/Array_3.java
@@ -106,7 +106,8 @@ public class Array_3 {
        return true;
     }
     
-    // Given n>=0, create an array length n*n with the following pattern, shown here for n=3 : {0, 0, 1,    0, 2, 1,    3, 2, 1} (spaces added to show the 3 groups).
+    // Given n>=0, create an array length n*n with the following pattern, shown 
+    here for n=3 : {0, 0, 1,    0, 2, 1,    3, 2, 1} (spaces added to show the 3 groups).
     
     
     
