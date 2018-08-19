80c9fb84ab1589b5937de695b94e7f67d3b7f88e
german <germanjongewaard@gmail.com>
2018-08-20 00:23:19 +0200

    Soluciones ejercicios CodingBat

--- src/codingbat/soluciones/Array_3.java
+++ src/codingbat/soluciones/Array_3.java
@@ -123,7 +123,7 @@ public class Array_3 {
 
     // Given n>=0, create an array with the pattern {1,    1, 2,    1, 2, 3,
     //... 1, 2, 3 .. n} (spaces added to show the grouping). Note that the 
-    length of the array will be 1 + 2 + 3 ... + n, which is known to sum to exactly n*(n + 1)/2.
+    //length of the array will be 1 + 2 + 3 ... + n, which is known to sum to exactly n*(n + 1)/2.
 
 
 
